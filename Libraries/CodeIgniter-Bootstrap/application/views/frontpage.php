e6895488e2bb538cdda609a928dbb13a802e7ec2
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-03-19 13:11:54 -0400

    added libraries

---
+++ Libraries/CodeIgniter-Bootstrap/application/views/frontpage.php
@@ -0,0 +1,29 @@
+   <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Recruited</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="signup.php">Sign Up!</a></li>
+              <li><a href="joblisting.php">Job Listing</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+  <div class="container">
+<br/>
+    <h1> Welcome to Recruited</h1>
+     <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper elit quis odio ultricies eget varius quam fringilla. Curabitur et tortor ut massa pulvinar iaculis quis sit amet elit. Mauris ac turpis in erat hendrerit semper. Duis ut tempus sapien. 
+     Quisque ultrices dui ut justo tincidunt ut ornare est tincidunt. Vivamus molestie lorem eu urna pellentesque quis mattis eros venenatis. Vestibulum vehicula ante eget magna faucibus pellentesque. Proin imperdiet sollicitudin eros, eget tincidunt felis vestibulum vel. Donec sit amet nulla tortor, vel dictum purus. In non est et augue faucibus gravida. Morbi iaculis ipsum nulla, vitae iaculis nisl. </article>
+     <ul class="nav">
+     	<li><a href="#">This is an example of a link in the body</a></li>
+    
+</div> <!-- /container -->

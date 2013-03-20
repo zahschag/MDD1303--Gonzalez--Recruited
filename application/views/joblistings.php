0f041e79ac97ff48690946e3acf7c37f7a1786f6
Zahscha Gonzalez <zahscha.gonzalez@gmail.com>
2013-03-20 13:18:48 -0400

    2.2 job listings view

---
+++ application/views/joblistings.php
@@ -0,0 +1,25 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Recruited</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li><a href="frontpage.php">Home</a></li>
+              <li><a href="signup.php">Sign Up!</a></li>
+              <li class="active"><a href="joblisting.php">Job Listing</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+<div class="container-fluid">
+
+	<h1>Job Listings</h1>
+	
+
+</div>
